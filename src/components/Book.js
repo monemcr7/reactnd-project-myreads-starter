7210531abc2620e94a78b228df1ffd16e83aafdb
Menam <monemcr7@gmail.com>
2022-06-09 00:18:07 +0200

    None seleted review

--- src/components/Book.js
+++ src/components/Book.js
@@ -27,7 +27,7 @@ class BookShelf extends Component {
 			<div className="book-shelf-changer">
 			  {book.shelf  && (	
 				  <select onChange={this.onBookShelfChange} defaultValue={book.shelf || 'none'}>
-					<option value="none" disabled>Move to...</option> 
+					<option disabled>Move to...</option> 
 					<option value="currentlyReading">Currently Reading</option>
 					<option value="wantToRead">Want to Read</option>
 					<option value="read">Read</option>
